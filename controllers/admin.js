8510ed09e86317781de994b44996b5bec9fac675
Leif <leifurcastle@gmail.com>
2024-09-11 16:18:29 -0700

    FEAT: added automatic emailing of users when their appointment has been cancelled

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,9 +5,43 @@ const express = require("express");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const moment = require('moment');
+const { CourierClient } = require("@trycourier/courier");
+
+const courier = new CourierClient({ authorizationToken: process.env.COURIER_API_KEY });
 
 const { Booking } = require("../models");
 const mongoose = require('mongoose');
+
+//--Send email to user after their appointment is cancelled
+const sendEmail = async (booking) => {
+    try {
+        const { requestId } = await courier.send({
+            message: {
+                to: {
+                    email: booking.info.contact.email
+                },
+                content: {
+                    title: `${booking.info.service.service} Appointment with 218 Tattoo Cancelled`,
+                    body: `${booking.info.contact.firstName} we're sorry to have to cancel your appointment with us on ${moment(booking.dateTime).format('MM/DD [at] hh:mm A')} We hope you'll look into trying our services again soon. 
+                    
+                    If you have any questions or would like to rebook with us please contact us at 360-443-1777, reply to this email, or visit our website to rebook.`,
+                },
+                data: {
+                    name: `${booking.info.contact.firstName} ${booking.info.contact.lastName}`,
+                },
+                routing: {
+                    method: "single", // Send through the default provider, which is Gmail SMTP
+                    channels: ["email"],
+                },
+            },
+        });
+
+        console.log("Email sent successfully with requestId: ", requestId);
+    } catch (error) {
+        console.error("Error sending email: ", error);
+    }
+};
 
 //--Login 
 router.post('/login', async (req, res) => {
@@ -25,10 +59,11 @@ router.post('/login', async (req, res) => {
 
 //----DELETE---- delete a booking appt
 router.post("/cancelBooking", (req, res) => {
-    console.log('Delete booking: ', req.body.id)
+    console.log('Delete booking id: ', req.body.id)
     Booking.findByIdAndDelete(req.body.id)
         .then((booking) => {
-            console.log('Deleted booking: ', booking); // Check if the booking is found
+            console.log('Deleted booking: ', booking); 
+            sendEmail(booking)
             res.status(200).send({message: 'Booking deleted'})
         })
         .catch((error) => {
