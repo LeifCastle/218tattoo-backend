17041f3569bd813bd0d308b41e2bf32a8b4d84e5
Leif <leifurcastle@gmail.com>
2024-09-28 19:22:46 -0700

    FIX: removed cors selective blocking

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const app = express();
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors({
-  origin: 'https://tattoo218-d1912baeabec.herokuapp.com', // Replace with your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
-}));
-
+app.use(cors());
 // Define Controllers
 app.use("/admin", require("./controllers/admin"));
 app.use('/book', require('./controllers/book'))
